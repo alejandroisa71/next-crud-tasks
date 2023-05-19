83beabbf121f42049860dd7f28b184f2b877f5ca
alejandro anibal isa <alejandroisa71@gmail.com>
2023-05-19 17:01:20 -0300

    First commit

---
+++ src/app/edit/[id]/page.jsx
@@ -0,0 +1,9 @@
+
+const page = ({params}) => {
+  return (
+    <div><h1>
+        editando {params.id}
+        </h1></div>
+  )
+}
+export default page
---
+++ src/app/new/page.jsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div>New</div>
+  )
+}
+export default page
