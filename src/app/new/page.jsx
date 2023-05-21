7724eabe57a037c615b0fb7184c25acc938c74de
Alejandro Anibal Isa <alejandroisa71@gmail.com>
2023-05-21 13:57:43 -0300

    Creating funcionallity addTasks

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,6 +1,32 @@
+'use client';
+import { useTasks } from '@/context/TaskContext';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 const page = () => {
+  const [task, setTask] = useState();
+  const { addTask } = useTasks();
+  const router = useRouter();
+
+  const handleChange = (e) =>
+    setTask({ ...task, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(task.title, task.description);
+    router.push('/')
+  };
+
   return (
-    <div>New</div>
-  )
-}
-export default page
+    <form onSubmit={handleSubmit}>
+      <input name="title" placeholder="Write a title" onChange={handleChange} />
+      <textarea
+        name="description"
+        placeholder="Write a description"
+        onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
+  );
+};
+export default page;
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,5 +1,6 @@
 'use client';
 const { createContext, useContext, useState } = require('react');
+import { v4 as uuid } from 'uuid';
 
 export const TaskContext = createContext();
 
@@ -11,25 +12,37 @@ export const useTasks = () => {
 };
 
 export const TaskProvider = ({ children }) => {
- const [tasks, setTasks]= useState([
-  {
-    id:1,
-    title:"mi first task",
-    description:'some one description',
-  },
-  {
-    id:2,
-    title:"mi second task",
-    description:'some second description',
-  },
-  {
-    id:3,
-    title:"mi third task",
-    description:'some third description',
-  },
- ])
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'mi first task',
+      description: 'some one description',
+    },
+    {
+      id: 2,
+      title: 'mi second task',
+      description: 'some second description',
+    },
+    {
+      id: 3,
+      title: 'mi third task',
+      description: 'some third description',
+    },
+  ]);
+
+  const addTask = (title, description) =>
+    setTasks([
+      ...tasks,
+      {
+        title,
+        description,
+        id: uuid,
+      },
+    ]);
 
   return (
-    <TaskContext.Provider value={{ tasks }}>{children}</TaskContext.Provider>
+    <TaskContext.Provider value={{ tasks, addTask }}>
+      {children}
+    </TaskContext.Provider>
   );
 };
